00409700be96238f453e297ce38837c649cf5370
Isti-k <kiscserkes03@gmail.com>
2024-04-04 15:33:45 +0200

    fuggolegesTeszt

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import { palya_valasztas, jatekKezdese } from "./fuggvenyek.js";
+import { fuggolegesTeszt } from "./fuggolegesTeszt.js";
 
 window.addEventListener("load", init);
 
 function init() {
   palya_valasztas();
   jatekKezdese();
+  fuggolegesTeszt();
 }
