e5112c190b0b7cb3d1b903ffe3d3b9b13be7d8ec
foldicintia <foldicintia2002@gmail.com>
2024-04-13 23:24:11 +0200

    átló tesztesetek

--- teszt.js
+++ teszt.js
@@ -1,8 +1,8 @@
 import { atlo_ell } from "./fuggvenyek.js";
 
 export function tesztAtlohoz() {
-    {
-      let lista = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
+    { // összes üres
+      let lista = [" ", " ", " ", " ", " ", " "," "," "," "];
       let vart = "   @   "; 
       console.assert(
         vart === atlo_ell(3, lista),
@@ -11,7 +11,7 @@ export function tesztAtlohoz() {
         "A várt eredmény nem stimmel!"
       );
     }
-    {
+    { // mind X
         let lista = ["X", "X", "X", "X", "X", "X", "X", "X", "X"];
         let vart = "XXX@XXX"; 
         console.assert(
@@ -21,9 +21,9 @@ export function tesztAtlohoz() {
           "A várt eredmény nem stimmel!"
         );
       }
-      {
-        let lista = ["O", "X", "X", "X", "X", "X", "X", " ", " "];
-        let vart = "OXX@XX  "; 
+      { // mind O
+        let lista = ["O", "O", "O", "O", "O", "O", "O", "O", "O"];
+        let vart = "OOO@OOO"; 
         console.assert(
           vart === atlo_ell(3, lista),
           "%o",
@@ -31,4 +31,45 @@ export function tesztAtlohoz() {
           "A várt eredmény nem stimmel!"
         );
       }
+      { // van győztes és nincs minden kitöltve
+        let lista = ["X", "X", "O", " ", "X", "O", "O", " ", "X"];
+        let vart = "XXX@OXO"; 
+        console.assert(
+          vart === atlo_ell(3, lista),
+          "%o",
+          `várt: ${vart}, lista: ${lista}`,
+          "A várt eredmény nem stimmel!"
+        );
+      }
+      { // van győztes és mind ki van töltve
+        let lista = ["O", "X", "X", "O", "O", "O", "X", "X", "O"];
+        let vart = "OOO@XOX"; 
+        console.assert(
+          vart === atlo_ell(3, lista),
+          "%o",
+          `várt: ${vart}, lista: ${lista}`,
+          "A várt eredmény nem stimmel!"
+        );
+      }
+      { // nincs győztes és nincs mind kitöltve
+        let lista = ["X", " ", "X", " ", "O", "O", " ", " ", " "];
+        let vart = "XO @XO "; 
+        console.assert(
+          vart === atlo_ell(3, lista),
+          "%o",
+          `várt: ${vart}, lista: ${lista}`,
+          "A várt eredmény nem stimmel!"
+        );
+      }
+      { // nincs győztes és mind ki van töltve
+        let lista = ["X", "O", "X", "O", "X", "O", "O", "X", "O"];
+        let vart = "XXO@XXO"; 
+        console.assert(
+          vart === atlo_ell(3, lista),
+          "%o",
+          `várt: ${vart}, lista: ${lista}`,
+          "A várt eredmény nem stimmel!"
+        );
+      }
+      
   }
