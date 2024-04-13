d80eddb5b78b4044e7d6712ec0df22c25fc84636
foldicintia <foldicintia2002@gmail.com>
2024-04-13 23:29:13 +0200

    átlós tesztesetek  importálva

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { palya_valasztas, jatekKezdese } from "./fuggvenyek.js";
-import { tesztAtlohoz } from "./teszt.js";
+import { tesztAtlohoz } from "./atloTeszt.js";
 
 import { tesztvizszintes } from "./teszt_vizszintes_ell.js";
 
